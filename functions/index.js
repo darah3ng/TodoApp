974e73bb75059861c62ea63885eca49cf27acfc6
Dara Heng <daragitgud@gmail.com>
2021-06-01 22:35:21 +1000

    feat: support update user details for BE

--- functions/index.js
+++ functions/index.js
@@ -12,7 +12,9 @@ const {
 const {
     loginUser,
     signUpUser,
-    uploadProfilePhoto
+    uploadProfilePhoto,
+    getUserDetail,
+    updateUserDetails
 } = require('./APIs/users');
 
 app.get('/todos', getAllTodos);
@@ -23,5 +25,7 @@ app.put('/todo/:todoId', editTodo);
 app.post('/login', loginUser);
 app.post('/signup', signUpUser);
 app.post('/user/image', auth, uploadProfilePhoto);
+app.get('/user', auth, getUserDetail);
+app.post('/user', auth, updateUserDetails);
 
 exports.api = functions.https.onRequest(app);
